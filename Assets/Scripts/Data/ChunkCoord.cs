93b4b86dc715345e5af2c64057952d56632824dc
Jerri Ahonen <jerri.ahonen@gmail.com>
2022-09-11 14:39:12 +0300

    Add ChunkCoords

---
+++ Assets/Scripts/Data/ChunkCoord.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+	public class ChunkCoord
+	{
+		public int X { get; }
+		public int Z { get; }
+
+		public Vector3 WorldPos => new(X * ChunkData.ChunkWidth, 0, Z * ChunkData.ChunkWidth);
+
+		public ChunkCoord(Vector2 coords) : this(Mathf.FloorToInt(coords.x), Mathf.FloorToInt(coords.y)) { }
+		public ChunkCoord(Vector2Int coords) : this(coords.x, coords.y) { }
+
+		public ChunkCoord(int x, int z)
+		{
+			X = x;
+			Z = z;
+		}
+
+		public override string ToString()
+		{
+			return $"Chunk ({X}, {Z})";
+		}
+	}
+}
