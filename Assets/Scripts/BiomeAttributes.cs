fa80ba61ab81a771f19070ba54a5e817ef60a696
Jerri Ahonen <jerri.ahonen@gmail.com>
2022-09-11 18:17:54 +0300

    Add biome attributes

---
+++ Assets/Scripts/BiomeAttributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Biomes/Attributes", fileName = "BiomeAttributes")]
+public class BiomeAttributes : ScriptableObject
+{
+	[SerializeField] private string biomeName;
+	[SerializeField] private int surfaceHeight;
+	[SerializeField] private int terrainHeight;
+	[SerializeField] private float terrainScale;
+	[SerializeField] private Lode[] lodes;
+	
+	public string Name => biomeName;
+	public int SurfaceHeight => surfaceHeight;
+	public int TerrainHeight => terrainHeight;
+	public float TerrainScale => terrainScale;
+	public Lode[] Lodes => lodes;
+}
