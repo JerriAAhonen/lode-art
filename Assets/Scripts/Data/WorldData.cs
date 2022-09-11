de89146700912ea301c053d4188b75b68437cf61
Jerri Ahonen <jerri.ahonen@gmail.com>
2022-09-11 14:39:22 +0300

    Add WorldData

---
+++ Assets/Scripts/Data/WorldData.cs
@@ -0,0 +1,18 @@
+﻿namespace Data
+{
+	public static class WorldData
+	{
+		public const int WorldSizeInChunks = 5;
+		
+		private static int worldSizeInVoxels;
+		public static int WorldSizeInVoxels
+		{
+			get
+			{
+				if (worldSizeInVoxels == 0)
+					worldSizeInVoxels = WorldSizeInChunks * ChunkData.ChunkWidth;
+				return worldSizeInVoxels;
+			}
+		}
+	}
+}
