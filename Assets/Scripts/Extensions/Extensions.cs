574016cc78f5e418dce0a8dc1e3623725efa0fbb
Jerri Ahonen <jerri.ahonen@gmail.com>
2022-09-11 14:39:37 +0300

    Add extension for Vector3

---
+++ Assets/Scripts/Extensions/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+	public static class Vector3Extensions
+	{
+		public static Vector3Int ToVector3Int(this Vector3 v3)
+		{
+			var x = Mathf.FloorToInt(v3.x);
+			var y = Mathf.FloorToInt(v3.y);
+			var z = Mathf.FloorToInt(v3.z);
+
+			return new Vector3Int(x, y, z);
+		}
+	}
+}
